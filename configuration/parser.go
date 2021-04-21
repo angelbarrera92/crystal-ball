0f1ea93258dd5885c5d3ae4ec0676a8816e2f958
Rirush <RaccoonRirush@gmail.com>
2021-04-21 14:50:26 +0800

    feat: add requests.yml file example, schema, and parser

---
+++ configuration/parser.go
@@ -0,0 +1,28 @@
+package configuration
+
+import (
+	"gopkg.in/yaml.v3"
+	"io"
+	"time"
+)
+
+// ParseRequests takes Reader, and uses yaml library to decode file into Requests struct
+func ParseRequests(file io.Reader) (*Requests, error) {
+	dec := yaml.NewDecoder(file)
+	dec.KnownFields(true)
+	r := &Requests{}
+	err := dec.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+	r.Timeout, err = time.ParseDuration(r.RawTimeout)
+	if err != nil {
+		return nil, err
+	}
+	r.DataFilter.Delay, err = time.ParseDuration(r.DataFilter.RawDelay)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: validate requests
+	return r, nil
+}
